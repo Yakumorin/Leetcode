ce400b3f61010d44b875023b119f3a9454e023ac
Yakumorin <chenpu13@mails.tsinghua.edu.cn>
2017-04-13 16:05:20 +0800

    Create 118. Pascal's Triangle.cpp

---
+++ 118. Pascal's Triangle.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int> > result;
+        for(int i=0; i<numRows; ++i) {
+            vector<int> ans;
+            ans.push_back(1);
+            for(int j=1; j<i; ++j)
+                ans.push_back(result[i-1][j-1] + result[i-1][j]);
+            if(i) ans.push_back(1);
+            result.push_back(ans);
+        }
+        return result;
+    }
+};

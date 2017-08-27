c9fd35a571640a5489138db41d6c99e42871c8f4
Yakumorin <puc1@uci.edu>
2017-08-27 15:43:52 +0800

    Create 198. House Robber.cpp

---
+++ 198. House Robber.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        if(!n) return 0;
+        int f[n+1];
+        f[0]=0;
+        f[1]=nums[0];
+        for(int i=2; i<=n; ++i)
+            f[i]=max(f[i-1],f[i-2]+nums[i-1]);
+        return f[n];
+    }
+};

17e9dc49ae1b9852ef83871bde67f975827c28b0
Yakumorin <puc1@uci.edu>
2017-09-03 20:44:47 +0800

    Create 384. Shuffle an Array.cpp

---
+++ 384. Shuffle an Array.cpp
@@ -0,0 +1,26 @@
+class Solution {
+    vector<int> origin, copies;
+public:
+    Solution(vector<int> nums) {
+        origin = nums;
+        copies = nums;
+    }
+    
+    /** Resets the array to its original configuration and return it. */
+    vector<int> reset() {
+        return origin;
+    }
+    
+    /** Returns a random shuffling of the array. */
+    vector<int> shuffle() {
+        random_shuffle(copies.begin(), copies.end());
+        return copies;
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(nums);
+ * vector<int> param_1 = obj.reset();
+ * vector<int> param_2 = obj.shuffle();
+ */
